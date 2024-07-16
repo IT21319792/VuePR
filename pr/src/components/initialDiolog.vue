13f7747f1150dce893d84176e129cd1d9c189a6e
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-16 11:39:16 +0530

    Create initialDiolog.vue

---
+++ pr/src/components/initialDiolog.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+            <h1>Ask a Question</h1>
+           <div class="mb=3">
+                <input
+                    name="question"
+                    type="text"
+                    class="form-control"
+                    id="question"
+                    placeholder="Enter your question"
+                />
+
+           </div>
+            <button @click="nextScreen" class="btn">Next</button>
+        </div>
+        </div>
+    </div>
+
+</div> 
+</template>
+
+<script>
+
+</script>
+
+
