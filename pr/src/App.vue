238de1e18ba270098eeeba0de39e50495b7d686b
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-16 11:38:57 +0530

    Create App.vue

---
+++ pr/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+main app
+<div class="container">
+
+</div>
+
+<component
+  :is="screens[position]"
+/>
+
+ 
+</template>
+
+<script>
+
+import appInitial from './components/initialDiolog.vue';
+import appConfirm from './components/confirmDiolog.vue';
+import appResults from './components/resultsDiolog.vue';
+
+export default{
+  components:{
+
+    appConfirm,
+    appInitial,
+    appResults
+
+
+
+  },
+  data(){
+    return{
+      screens:['appInitial','appConfirm','appResults'],
+      position:0
+    }
+
+  }
+
+}
+
+</script>
+
+<style>
+  @import './assets/style.css';
+</style>
